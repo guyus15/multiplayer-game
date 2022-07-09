5da1f3b9899f8ceffbba6231de92e0c70b2affa7
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2022-07-09 17:26:31 +0100

    Removed docs directory

--- docs/html/search/all_1.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['packet_2ec_0',['packet.c',['../packet_8c.html',1,'']]],
-  ['packet_2eh_1',['packet.h',['../packet_8h.html',1,'']]],
-  ['packet_5fstruct_2',['packet_struct',['../structpacket__struct.html',1,'']]]
-];
--- docs/html/search/all_2.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['read_5fbyte_0',['read_byte',['../packet_8c.html#a11b4d5d00a22b5d4dbeff55b3b419911',1,'read_byte(packet_t *packet, char *dest):&#160;packet.c'],['../packet_8h.html#a11b4d5d00a22b5d4dbeff55b3b419911',1,'read_byte(packet_t *packet, char *dest):&#160;packet.c']]],
-  ['read_5fdouble_1',['read_double',['../packet_8c.html#a9870f3ee21af976a242a6ff3490ca752',1,'read_double(packet_t *packet, double *dest):&#160;packet.c'],['../packet_8h.html#a9870f3ee21af976a242a6ff3490ca752',1,'read_double(packet_t *packet, double *dest):&#160;packet.c']]],
-  ['read_5ffloat_2',['read_float',['../packet_8c.html#a0a54a2374f23563eecd1916f4e9dd776',1,'read_float(packet_t *packet, float *dest):&#160;packet.c'],['../packet_8h.html#a0a54a2374f23563eecd1916f4e9dd776',1,'read_float(packet_t *packet, float *dest):&#160;packet.c']]],
-  ['read_5fint16_3',['read_int16',['../packet_8c.html#aabab5f191f43074a0bc9071736fdeacb',1,'read_int16(packet_t *packet, int16_t *dest):&#160;packet.c'],['../packet_8h.html#aabab5f191f43074a0bc9071736fdeacb',1,'read_int16(packet_t *packet, int16_t *dest):&#160;packet.c']]],
-  ['read_5fint32_4',['read_int32',['../packet_8c.html#a21e5a21c4fbcbeb2d450dc371bd91bb1',1,'read_int32(packet_t *packet, int32_t *dest):&#160;packet.c'],['../packet_8h.html#a21e5a21c4fbcbeb2d450dc371bd91bb1',1,'read_int32(packet_t *packet, int32_t *dest):&#160;packet.c']]],
-  ['read_5fint64_5',['read_int64',['../packet_8c.html#acb36a0202645ae8f675d6cd5fba4fc77',1,'read_int64(packet_t *packet, int64_t *dest):&#160;packet.c'],['../packet_8h.html#acb36a0202645ae8f675d6cd5fba4fc77',1,'read_int64(packet_t *packet, int64_t *dest):&#160;packet.c']]]
-];
