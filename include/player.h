896d01f949c7b279e4ad178b1e8395a605c97902
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2022-07-26 21:01:27 +0100

    Included cglm in the player to use for player position and velocity

--- include/player.h
+++ include/player.h
@@ -3,14 +3,18 @@
 
 #define PLAYER_NAME 20
 
+#include <cglm/cglm.h>
+
 typedef struct player_type
 {
     int id;
     char username[PLAYER_NAME];
+    vec2 position;
+    vec2 velocity;
 } player_t;
 
 player_t *create_player();
 
-void move_player(char *inputs);
+void move_player(player_t *player, char *inputs);
 
 #endif
