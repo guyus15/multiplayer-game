05c32fc1cd62078c6c338b2f8e9dc51ed5b2e8b3
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2022-08-08 21:35:07 +0100

    Added declarations for load_texture and get_texture

--- include/rendering/resource_manager.h
+++ include/rendering/resource_manager.h
@@ -2,10 +2,16 @@
 #define RESOURCE_MANAGER_H
 
 #include <rendering/shader.h>
+#include <rendering/texture.h>
 
 shader_t *load_shader(const char *name,
                       const char *vertex_shader_path,
                       const char *fragment_shader_path);
 shader_t *get_shader(const char *name);
+
+texture_t *load_texture(const char *name,
+                        const char *file_path,
+                        int alpha);
+texture_t *get_texture(const char *name);
 
 #endif
