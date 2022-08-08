59e68b30e0f245bfe72ddc89a5db9df97a48b5d4
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2022-08-08 19:07:46 +0100

    Added declarations for setting shader uniforms for each required data type

--- include/rendering/shader.h
+++ include/rendering/shader.h
@@ -1,6 +1,7 @@
 #ifndef SHADER_H
 #define SHADER_H
 
+#include <stdbool.h>
 #include <cglm/cglm.h>
 
 typedef struct shader_type
@@ -12,5 +13,10 @@ void compile_shader(shader_t *shader,
                     const char *vertex_shader_source,
                     const char *fragment_shader_source);
 void use_shader(shader_t *shader);
+void set_bool(shader_t *shader, const char *name, int value);
+void set_int(shader_t *shader, const char *name, int value);
+void set_float(shader_t *shader, const char *name, float value);
+void set_mat4(shader_t *shader, const char *name, mat4 value);
+void set_vec3(shader_t *shader, const char *name, vec3 value);
 
 #endif
